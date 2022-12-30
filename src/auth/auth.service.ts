75b622be4bb6116d6596ffa7449af30cf63ec344
yksolanki9 <yashsolanki1709@gmail.com>
2022-12-30 19:51:41 +0530

    Generate access token and implement jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,20 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import * as argon from 'argon2';
+
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     try {
@@ -21,11 +29,7 @@ export class AuthService {
         },
       });
 
-      //This will only delete the hash field from the user object. Nothing changes in DB.
-      delete user.hash;
-
-      //Send user
-      return user;
+      return await this.generateToken(user.id, user.email);
     } catch (err) {
       if (err instanceof PrismaClientKnownRequestError) {
         if (err.code === 'P2002') {
@@ -57,8 +61,26 @@ export class AuthService {
       throw new ForbiddenException('Incorrect password');
     }
 
-    //Return user
-    delete user.hash;
-    return user;
+    return await this.generateToken(user.id, user.email);
+  }
+
+  async generateToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const JWT_SECRET = this.config.get('JWT_SECRET');
+
+    const jwtToken = await this.jwtService.signAsync(payload, {
+      secret: JWT_SECRET,
+      expiresIn: '15min',
+    });
+
+    return {
+      access_token: jwtToken,
+    };
   }
 }
