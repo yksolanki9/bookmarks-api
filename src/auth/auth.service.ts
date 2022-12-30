be57f08d59f949ef24972dc97bb32a7bc5fc81be
yksolanki9 <yashsolanki1709@gmail.com>
2022-12-30 16:59:40 +0530

    Add auth dto with data validation and add logic for signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
-  signup() {
-    return 'Sign UP route';
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    try {
+      //Create password hash
+      const hash = await argon.hash(dto.password);
+
+      //Store user
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      //This will only delete the hash field from the user object. Nothing changes in DB.
+      delete user.hash;
+
+      //Send user
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('User already exists');
+        }
+      }
+      throw err;
+    }
   }
 
   signin() {
